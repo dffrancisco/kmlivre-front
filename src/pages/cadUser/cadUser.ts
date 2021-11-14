ee4e3fc858aca4b2d581b295961f6c91bec3dd47
DolbsK <kallebealves37@gmail.com>
2021-11-14 20:16:18 -0300

    start Cadastro de usuario

---
+++ src/pages/cadUser/cadUser.ts
@@ -0,0 +1,16 @@
+import { reactive } from 'vue'
+import axios from 'axios'
+
+export const state = reactive({
+
+
+})
+
+export const actions = {
+
+}
+
+export default {
+    state,
+    actions
+}
---
+++ src/pages/cadUser/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+//@ts-ignore
+import { actions, state } from "./cadUser"
+//@ts-ignore
+
+
+
+</script>
+
+<template>
+    <div class="principal">
+        <div>
+            <div class="card">aaaaaaa</div>
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.principal {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+}
+.card {
+    background-color: #b010e6;
+    height: 150px;
+    width: 280px;
+    margin-right: 25px;
+    margin-top: 15px;
+    border-top: #cc1efc solid 20px;
+    box-shadow: 4px 2px 2px #444444bf;
+    color: black;
+}
+</style>
