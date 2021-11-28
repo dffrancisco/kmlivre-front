111685cbfdd197b6137dac4558600703652081ca
dffrancisco <dffrancisco@gmail.com>
2021-11-28 20:14:26 -0300

    on click notifiation

--- src/sw.ts
+++ src/sw.ts
@@ -16,29 +16,44 @@ self.addEventListener('push', e => {
   self.registration.showNotification(data.title, {
     body: data.message,
     vibrate: [100, 50, 100],
-    icon: './icon/icon-km-128.png'
+    icon: './icon/icon-km-128.png',
+    badge: './icon/icon-km-128.png',
+    image: './icon/icon-km-128.png',
   });
-
-  console.log(data)
-  console.log('Notification Received');
-
-  // e.waitUntil(
-  //   //@ts-ignore
-  //   self.regenerate.showNotification(data.title, {
-  //     body: data.message,
-  //     icon: './icon/icon-km-128.png',
-  //     vibrate: [100, 50, 100],
-  //     actions: [
-  //       { action: 'abrir', title: 'Abir', icon: 'https://cdn.iconscout.com/icon/free/png-256/art-museum-painting-palette-38814.png' },
-  //       { action: 'close', title: 'Fechar', icon: './icon/icon-km-128.png' }
-  //     ]
-
-  //   }
-  //   )
-  // )
 
 });
 
+self.addEventListener('notificationclick', event => {
+  const rootUrl = new URL('/', location).href;
+  event.notification.close();
+  // Enumerate windows, and call window.focus(), or open a new one.
+  event.waitUntil(
+    clients.matchAll().then(matchedClients => {
+      for (let client of matchedClients) {
+        if (client.url === rootUrl) {
+          return client.focus();
+        }
+      }
+      return clients.openWindow("/");
+    })
+  );
+});
+
+
+// self.addEventListener('notificationclick', function (event) {
+//   const rootUrl = new URL('./', location).href;
+//   event.notification.close();
+//   event.waitUntil(
+//     clients.matchAll().then(matchedClients => {
+//       for (let client of matchedClients) {
+//         if (client.url.indexOf(rootUrl) >= 0) {
+//           return client.focus();
+//         }
+//       }
+//       return clients.openWindow(rootUrl).then(function (client) { client.focus(); });
+//     })
+//   );
+// });
 
 // self.__WB_MANIFEST is default injection point
 precacheAndRoute(self.__WB_MANIFEST)
